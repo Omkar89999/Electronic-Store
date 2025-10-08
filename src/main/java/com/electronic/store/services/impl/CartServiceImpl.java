22693db20d4dd3db1741bd0a975e1c65f81ce292
Omkar Ghodechor <ghodechoromkar@gmail.com>
2025-10-08 08:33:39 +0530

    cart

--- src/main/java/com/electronic/store/services/impl/CartServiceImpl.java
+++ src/main/java/com/electronic/store/services/impl/CartServiceImpl.java
@@ -1,9 +1,12 @@
 package com.electronic.store.services.impl;
 
 import java.sql.Date;
-import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
+import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,7 @@ import com.electronic.store.entity.Cart;
 import com.electronic.store.entity.CartItem;
 import com.electronic.store.entity.Product;
 import com.electronic.store.entity.User;
+import com.electronic.store.repository.CartItemRepository;
 import com.electronic.store.repository.CartRepository;
 import com.electronic.store.repository.ProductRepository;
 import com.electronic.store.repository.UserRepository;
@@ -30,6 +34,9 @@ public class CartServiceImpl implements CartService {
 
     @Autowired
     private CartRepository cartRepository;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
 
     @Autowired
     private ModelMapper mapper;
@@ -54,20 +61,54 @@ public class CartServiceImpl implements CartService {
 
         } catch (NoSuchElementException e) {
             cart = new Cart();
+
+            cart.setCartId(UUID.randomUUID().toString());
             cart.setCreatedAt(new Date(System.currentTimeMillis()));
         }
 
         // perform cart operations
+        // if cart already present then update it.
 
+        // boolean updated = false;
+        AtomicReference<Boolean> updated = new AtomicReference<>(false);
         List<CartItem> items = cart.getItems();
-        // create a new cart item
-        CartItem.builder().quantity(quantity).product(product).build();
+        
+        List<CartItem> updatedItems = items.stream().map(item -> {
+            if (item.getProduct().getProductId().equals(productId)) {
+                // item already present in cart
+                item.setQuantity(quantity);
+                item.setTotalPrice(quantity * product.getPrice());
 
-        return null;
+                // updated = true;
+                updated.set(true);
+            }
+            return item;
+
+        }).collect(Collectors.toList());
+
+        cart.setItems(updatedItems);
+
+        if (!updated.get()) {
+            // item not present in cart
+            // create a new cart item
+            CartItem cartItem = CartItem.builder().quantity(quantity).totalPrice(quantity * product.getPrice())
+                    .cart(cart)
+                    .product(product).build();
+
+            cart.getItems().add(cartItem);
+
+        }
+
+        cart.setUser(user);
+        Cart updatedCart = cartRepository.save(cart);
+
+        return mapper.map(updatedCart, CartDto.class);
     }
 
     @Override
     public void clearCart(String userId) {
+
+        
 
     }
 
@@ -77,6 +118,3 @@ public class CartServiceImpl implements CartService {
     }
 
 }
-
-
-
